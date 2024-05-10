9241bab4b2430a07cf6ace89c896064dd0a904d4
Francisco Wallison <franciscowallison@gmail.com>
2024-05-10 19:04:13 -0300

    ADS: 123456789 REST-cli

--- pages/api/cliente.js
+++ pages/api/cliente.js
@@ -1,3 +1,17 @@
+/**
+ * Nome do arquivo: exemplo.js
+ * Data de criação: 30/04/2024
+ * Autor: João Silva
+ * Matrícula: 123456
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por implementar as funcionalidades
+ * de interação do usuário com a interface gráfica do módulo de vendas.
+ * Aqui são tratados eventos de cliques, validações de entrada e comunicação
+ * com APIs para envio e recebimento de dados.
+ *
+ * Este script é parte o curso de ADS.
+ */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import openDB from "../../database/dbConfig";
 
